75005a25e4d0ecd213b964e642f591b2940a49f1
Zhao Zhu <John_Chu@outlook.com>
2020-11-05 23:56:56 -0600

    init commit: fullstack calculator

---
+++ client/src/Calculator.js
@@ -0,0 +1,173 @@
+import React from "react";
+
+import Engine from "./Engine.js";
+
+import Display from "./Display.js";
+import Button from "./Button.js";
+
+import "./Calculator.css";
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            display: "0",
+            log: [],
+            engine: new Engine(),
+        };
+
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            log: this.state.engine.retrieve()
+        });
+    }
+
+    handleButtonClick(value) {
+        this.setState({
+            display: this.state.engine.calculate(value),
+            log: this.state.engine.retrieve()
+        });
+    }
+
+    render() {
+        const data = this.state.log;
+        return (
+            <div>
+                <div className="Calculator">
+
+                    <Display display={this.state.display} />
+
+                    <Button
+                        value={this.state.engine.clearable ? "C" : "AC"}
+                        className="Button BlackButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="+/-"
+                        className="Button BlackButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="%"
+                        className="Button BlackButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value={"\u00F7"}
+                        className="Button OrangeButton"
+                        onClick={this.handleButtonClick}
+                    />
+
+                    <br />
+
+                    <Button
+                        value="7"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="8"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="9"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="x"
+                        className="Button OrangeButton"
+                        onClick={this.handleButtonClick}
+                    />
+
+                    <br />
+
+                    <Button
+                        value="4"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="5"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="6"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="-"
+                        className="Button OrangeButton"
+                        onClick={this.handleButtonClick}
+                    />
+
+                    <br />
+
+                    <Button
+                        value="1"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="2"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="3"
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="+"
+                        className="Button OrangeButton"
+                        onClick={this.handleButtonClick}
+                    />
+
+                    <br />
+
+                    <Button
+                        value="0"
+                        className="Button GrayButton LargeButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="."
+                        className="Button GrayButton"
+                        onClick={this.handleButtonClick}
+                    />
+                    <Button
+                        value="="
+                        className="Button OrangeButton"
+                        onClick={this.handleButtonClick}
+                    />
+                </div>
+
+                <div className="container">
+                    <div id="result" className="Logs">
+                        <ul>
+                            {data.length <= 0
+                                ? 'No Computations Yet'
+                                : data.map((dat) => (
+                                    <li key={dat.id}>
+                                        <span> history > </span>
+                                        {dat.message}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Calculator;
